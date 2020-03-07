3c0f63f8466001d0819e5f0637465f66c422091f
vinibispo <vini.bispo015@gmail.com>
2020-03-07 16:08:25 -0300

    adding mongo and nodemon to million

--- models/Admin.js
+++ models/Admin.js
@@ -1,6 +1,7 @@
-const Admin = {
-    nome: "Danilo",
-    senha: "123",
-    first: () => "Danilo"
-}
+const mongoose = require('../database')
+const Admin = mongoose.model('user', { 
+    nome: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    senha: { type: String, required: true },
+  });
 module.exports = Admin
